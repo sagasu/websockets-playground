e72e4323751808ba50436c819ee87850b726f441
Mateusz Kopij <sagasu@gmail.com>
2021-12-05 22:49:53 +0000

    updates to project

--- csharp/WebSockets/WebSocketsServer/Startup.cs
+++ csharp/WebSockets/WebSocketsServer/Startup.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.WebSockets;
+using System.Threading;
 
 namespace WebSocketsServer
 {
@@ -26,16 +27,55 @@ namespace WebSocketsServer
 
             app.Use(async (context, next) =>
             {
+                WriteRequestParam(context);
                 if (context.WebSockets.IsWebSocketRequest)
                 {
                     var webSocket = await context.WebSockets.AcceptWebSocketAsync();
                     Console.WriteLine("WebSocket connected");
+
+                    await ReceiveMessage(webSocket, async (result, buffer) =>
+                    {
+
+                    });
                 }
                 else
                 {
+                    Console.WriteLine("This is not WebSocket request");
+                    // If it is not web socket, check with next middleware in a request pipeline.
                     await next();
                 }
             });
+
+            app.Run(async context =>
+            {
+                Console.WriteLine("Run delegate");
+                await context.Response.WriteAsync("Hi from run delegate");
+                
+            });
+        }
+
+        public void WriteRequestParam(HttpContext context)
+        {
+            Console.WriteLine($"method: ${context.Request.Method}");
+            Console.WriteLine($"protocol: ${context.Request.Protocol}");
+            
+            if(context.Request.Headers != null)
+                foreach (var requestHeader in context.Request.Headers)
+                {
+                    Console.WriteLine($"--> {requestHeader.Key}: {requestHeader.Value}");
+                }
+        }
+
+        private async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(new ArraySegment<byte>(), CancellationToken.None);
+
+                handleMessage(result, buffer);
+            }
         }
     }
 }
